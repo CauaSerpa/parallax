603014b907b06d1197f63d5bf1e25f6f72ea05cd
Cauã Serpa <91634425+CauaSerpa@users.noreply.github.com>
2022-04-28 21:32:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
@@ -9,7 +8,7 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    stars.style.left = value * .25 + 'px';
+    
     moon.style.top = value * 1.05 + 'px';
     mountains_behind.style.top = value * .5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
